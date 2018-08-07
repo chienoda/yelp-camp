bdf67f2094c874c98d690cc5a0c57907f44b8782
Chie Noda <chienoda119@gmail/com>
2018-08-07 21:45:02 +0000

    Add mongoose

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express    = require('express'),
+    app        = express(),
+    bodyParser = require('body-parser'),
+    mongoose   = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set('view engine', 'ejs');
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: 'Turkey Point',
+//         image: 'https://recreation-acm.activefederal.com/assetfactory.aspx?did=7656'
+        
+//     },
+//     function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log('NEWLY CREATED CAMPGROUND: ');
+//             console.log(campground);
+//         }
+//         });
 
 var campgrounds = [
         {name: 'Pinary', image: 'http://www.env.gov.bc.ca/bcparks/explore/parkpgs/whiskers/whiskers.jpg'},
@@ -22,8 +47,6 @@ var campgrounds = [
         {name: 'Pinary', image: 'http://www.env.gov.bc.ca/bcparks/explore/parkpgs/whiskers/whiskers.jpg'},
         {name: 'Turkey Point', image: 'https://recreation-acm.activefederal.com/assetfactory.aspx?did=7656'},
         {name: 'Long Point', image: 'http://vacationingwithkids.com/wp-content/uploads/2014/08/10520895_10154422250285646_5717167271521392684_n-300x224.jpg'},
-        
-        
 ];
 
 app.get('/', function(req, res){
@@ -31,17 +54,30 @@ app.get('/', function(req, res){
 });
 
 app.get('/campgrounds', function(req, res){
-    res.render('campgrounds', {campgrounds: campgrounds});
+    //Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('campgrounds', {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 app.post('/campgrounds', function(req, res){
-    //get data from form and add to campground array
-    //redirect back to campground page
+    // get data from form and add to campground array
+    // redirect back to campground page
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect('/campgrounds');
+    var newCampground = {name: name, image: image}
+    // create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect('/campgrounds');
+        }
+    });
 });
 
 app.get('/campgrounds/new', function(req, res){
