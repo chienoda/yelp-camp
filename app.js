18c6a212e4c4cbc49bfe5223faf8caf7b0d26249
Chie Noda <chienoda119@gmail/com>
2018-08-13 20:16:08 +0000

    Add post route and a button to add new comment

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express    = require("express"),
     bodyParser = require("body-parser"),
     mongoose   = require("mongoose"),
     Campground = require("./models/campground"),
-    seedDB = require("./seeds")
+    Comment    = require("./models/comment"),
+    seedDB     = require("./seeds")
     
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
@@ -74,6 +75,27 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
             res.render("comments/new", {campground: campground});
         }
     });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    // find campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+    
 });
 
 
