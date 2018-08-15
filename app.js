b583bf389abab017b9058ecc0dac1c613c498a9c
Chie Noda <chienoda119@gmail/com>
2018-08-15 21:39:25 +0000

    Add login route and login template

--- app.js
+++ app.js
@@ -135,7 +135,17 @@ app.post("/register", function(req, res) {
     });
 });
 
-
+// show login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+// handling login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
 
 
 
