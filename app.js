af41580fb937cadeb1b3a56e3059518fb4e9a2ee
Chie Noda <chienoda119@gmail/com>
2018-08-16 01:07:08 +0000

    Add logout route

--- app.js
+++ app.js
@@ -147,6 +147,12 @@ app.post("/login", passport.authenticate("local",
     }), function(req, res){
 });
 
+// LOGOUT ROUTE
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
