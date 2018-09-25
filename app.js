2df730aedc4db5b11751bdcea209bb75fd29765c
Chie Noda <chienoda119@gmail/com>
2018-09-25 14:49:26 +0000

    update dburl

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ var express       = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index")
-    
-// mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
-// mongodb://chie:haru0828@ds111963.mlab.com:11963/yelp-camp
-mongoose.connect("mongodb://chie:haru0828@ds111963.mlab.com:11963/yelp-camp", { useNewUrlParser: true });
+
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
+mongoose.connect(url, { useNewUrlParser: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
